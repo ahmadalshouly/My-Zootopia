4811b0aa671685b6d5801e47e3df3ebb1f663710
ahmadshouly <ajood19@gmail.com>
2026-05-26 12:58:42 +0200

    skript erweitert und html erstellt

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,18 +7,28 @@ def load_data(file_path):
 
 animals_data = load_data("animals_data.json")
 
-
+output = ""
 for animal in animals_data:
     if "name" in animal:
-        print(f"Name: {animal['name']}")
+        output += f"Name: {animal['name']}\n"
 
     if "characteristics" in animal and"diet" in animal["characteristics"]:
-        print(f"Diet: {animal['characteristics']['diet']}")
+        output += f"Diet: {animal['characteristics']['diet']}\n"
 
     if "locations" in animal and len(animal["locations"])>0:
-        print(f"Location: {animal['locations'][0]}")
+        output += f"Locations: {animal['locations'][0]}\n"
 
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        print(f"Type: {animal['characteristics']['type']}")
+        output += f"Type: {animal['characteristics']['type']}\n"
 
-    print()
+    output += "\n"
+
+with open("animals_template.html", "r") as template_file:
+    template_content = template_file.read()
+
+final_template =template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as output_file:
+    output_file.write(final_template)
+
+print("Animals Web Generator has been successfully created")
