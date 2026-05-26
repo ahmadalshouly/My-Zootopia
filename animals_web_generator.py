94e8d2e2f4cd7ae3415243c1b458ec962dccd774
ahmadshouly <ajood19@gmail.com>
2026-05-26 13:11:18 +0200

    Kartenelemente für jedes Tier erstellt

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,19 +9,20 @@ animals_data = load_data("animals_data.json")
 
 output = ""
 for animal in animals_data:
+    output += '<li class="cards__item">'
     if "name" in animal:
-        output += f"Name: {animal['name']}\n"
+        output += f"Name: {animal['name']}</br>\n"
 
     if "characteristics" in animal and"diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
+        output += f"Diet: {animal['characteristics']['diet']}</br>\n"
 
     if "locations" in animal and len(animal["locations"])>0:
-        output += f"Locations: {animal['locations'][0]}\n"
+        output += f"Locations: {animal['locations'][0]}</br>\n"
 
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}\n"
+        output += f"Type: {animal['characteristics']['type']}</br>\n"
 
-    output += "\n"
+    output += "</li>\n"
 
 with open("animals_template.html", "r") as template_file:
     template_content = template_file.read()
