f155b4986e4dab011dfd6c6c08d22c2e8932fc9d
ahmadshouly <ajood19@gmail.com>
2026-05-26 16:15:22 +0200

    Code refaktoriert und mehr Felder hinzugefügt

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,38 +2,68 @@ import json
 
 def load_data(file_path):
   """ Loads a JSON file """
-  with open(file_path, "r") as handle:
+  with open(file_path, "r", encoding="utf-8") as handle:
     return json.load(handle)
 
-animals_data = load_data("animals_data.json")
+def serialize_animals(animals):
+    """ Serializes animals and their info """
+    output = '<li class="cards__item">'
 
-output = ""
-for animal in animals_data:
-    output += '<li class="cards__item">'
-
-    if "name" in animal:
-        output += f'<div class="card__title">{animal["name"]}</div>\n'
+    if "name" in animals:
+        output += f'<div class="card__title">{animals["name"]}</div>\n'
 
     output += '  <p class="card__text">\n'
 
-    if "characteristics" in animal and"diet" in animal["characteristics"]:
-        output += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+    if 'taxonomy' in animals and 'kingdom' in animals["taxonomy"]:
+        kingdom = animals["taxonomy"]["kingdom"]
+        output+= f'       <strong>Kingdom:</strong> {kingdom}<br/>\n'
 
-    if "locations" in animal and len(animal["locations"])>0:
-        output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+    if 'taxonomy' in animals and 'scientific_name' in animals["taxonomy"]:
+        scientific_name = animals["taxonomy"]["scientific_name"]
+        output += f'      <strong>Scientific name:</strong> {scientific_name}<br/>\n'
 
-    if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f'      <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+    if "characteristics" in animals and"diet" in animals["characteristics"]:
+        diet = animals["characteristics"]["diet"]
+        output += f'      <strong>Diet:</strong> {diet}<br/>\n'
+
+    if "locations" in animals and len(animals["locations"])>0:
+        output += f'      <strong>Location:</strong> {animals["locations"][0]}<br/>\n'
+
+    if "characteristics" in animals and "type" in animals["characteristics"]:
+        animal_type = animals["characteristics"]["type"]
+        output += f'      <strong>Type:</strong> {animal_type}<br/>\n'
 
     output += '  </p>\n'
     output += "</li>\n"
+    return output
 
-with open("animals_template.html", "r") as template_file:
-    template_content = template_file.read()
+def generate_animal_html(data, template_path, output_path):
+    """ Generates HTML page for animals """
+    output = ""
+    for animal in data:
+        output += serialize_animals(animal)
+    with open(template_path, "r", encoding="utf-8") as template_file:
+        template_content = template_file.read()
 
-final_template =template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+    # add encoding to head
+    if '<meta charset="UTF-8">' not in template_content:
+        template_content =template_content.replace(
+            "<head>",
+            "<head> \n<meta charset='UTF-8'>"
+        )
 
-with open("animals.html", "w") as output_file:
-    output_file.write(final_template)
+    # replace the content with our output
+    final_template = template_content.replace("__REPLACE_ANIMALS_INFO__",output)
 
-print("Animals Web Generator has been successfully created")
+    #create the animals.html file
+    with open(output_path, "w", encoding="utf-8") as output_file:
+        output_file.write(final_template)
+    print("Animals Web Generator has been successfully created")
+
+def main():
+    """ Main function """
+    animals_data = load_data("animals_data.json")
+    generate_animal_html(animals_data, "animals_template.html", "animals.html")
+
+if __name__ == "__main__":
+    main()
